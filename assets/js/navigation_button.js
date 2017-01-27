516b2ec8608d2dd9fe217b966f526d0a16ed9cab
WFJ Cuizon <justincuizon@gmail.com>
2017-01-27 20:03:50 +0800

    added a functioning dropdown menu

--- assets/js/navigation_button.js
+++ assets/js/navigation_button.js
@@ -1,11 +1,22 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function toggleMenu(){
-	var mobileNav = document.getElementsByClass('header_navigation_mobile');
-	if(mobileNav.style.display == "none"){
-		mobileNav.style.display == "block";
+function toggleMenu() {
+	document.getElementById("mobile_Nav").classList.toggle("is_open");
+}
+
+window.onclick = function(event) {
+	if(!event.target.matches('.mobile_navigation_button')) {
+
+		var dropdowns = document.getElementsByClassName("mobile_Nav_content");
+		var i;
+		for(i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if(openDropdown.classList.contains('is_open')) {
+				openDropdown.classList.remove('is_open');
+			}
+		}
+
 	}
-	else{
-		mobileNav.display = "none";
-	}
+
+	
 }
